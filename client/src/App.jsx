10a4362be4da0c88b6c3431f456ef1bf72ad59c8
Syed Mohammad Yasir <smy329@gmail.com>
2023-10-01 18:55:05 +0600

    sign in completely functional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Signin from './pages/Signin';
+import Signin from './pages/SignIn';
 import Signup from './pages/SignUp';
 import About from './pages/About';
 import Header from './components/Header';
