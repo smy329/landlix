49fe6db6725648d643abd4b8bfffe64bf5bc568b
Syed Mohammad Yasir <smy329@gmail.com>
2023-10-01 18:51:40 +0600

    create sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signupController = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
@@ -8,6 +9,34 @@ export const signupController = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json('User created successfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signinController = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
+    }
+
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, 'Wrong credencials'));
+    }
+
+    //generate token and store it in cookie
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc; //we need to desctucture the validUser._doc. validUser have a lot of data
+    res
+      .cookie('access_token', token, {
+        httpOnly: true, // Helps protect against cross-site scripting (XSS) attacks
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signupController } from '../controller/auth.controller.js';
+import { signinController, signupController } from '../controller/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/signup', signupController);
+router.post('/signin', signinController);
 
 export default router;
